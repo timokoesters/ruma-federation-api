3b9b1c76772daf5f2e0ffc169771685f1d31a473
timokoesters <timo@koesters.xyz>
2020-04-25 13:44:19 +0200

    Use RoomNetwork struct in publicRooms endpoint

--- src/v1/get_public_rooms.rs
+++ src/v1/get_public_rooms.rs
@@ -3,7 +3,12 @@
 use js_int::UInt;
 use ruma_api::ruma_api;
 use ruma_identifiers::{RoomAliasId, RoomId};
-use serde::{Deserialize, Serialize};
+use serde::{
+    de::{MapAccess, Visitor},
+    ser::SerializeStruct,
+    Deserialize, Deserializer, Serialize, Serializer,
+};
+use std::fmt;
 
 ruma_api! {
     metadata {
@@ -24,23 +29,20 @@ ruma_api! {
         #[serde(skip_serializing_if = "Option::is_none")]
         #[ruma_api(query)]
         pub since: Option<String>,
-        /// Whether or not to include all networks/protocols defined by application services on the
-        /// homeserver. Defaults to false.
-        #[serde(skip_serializing_if = "Option::is_none")]
+        /// Network to fetch the public room lists from.
+        #[serde(flatten, skip_serializing_if = "ruma_serde::is_default")]
         #[ruma_api(query)]
-        pub include_all_networks: Option<bool>,
-        /// The specific third party network/protocol to request from the homeserver. Can only be used if include_all_networks is false.
-        #[serde(skip_serializing_if = "Option::is_none")]
-        #[ruma_api(query)]
-        pub third_party_instance_id: Option<String>,
+        pub room_network: RoomNetwork,
     }
 
     response {
         /// A paginated chunk of public rooms.
         pub chunk: Vec<PublicRoomsChunk>,
         /// A pagination token for the response.
+        #[serde(skip_serializing_if = "Option::is_none")]
         pub next_batch: Option<String>,
         /// A pagination token that allows fetching previous results.
+        #[serde(skip_serializing_if = "Option::is_none")]
         pub prev_batch: Option<String>,
         /// An estimate on the total number of public rooms, if the server has an estimate.
         pub total_room_count_estimate: Option<UInt>,
@@ -75,4 +77,100 @@ pub struct PublicRoomsChunk {
     /// The URL for the room's avatar, if one is set.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub avatar_url: Option<String>,
+}
+
+/// Information about which networks/protocols from application services on the
+/// homeserver from which to request rooms.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum RoomNetwork {
+    /// Return rooms from the Matrix network.
+    Matrix,
+    /// Return rooms from all the networks/protocols the homeserver knows about.
+    All,
+    /// Return rooms from a specific third party network/protocol.
+    ThirdParty(String),
+}
+
+impl Default for RoomNetwork {
+    fn default() -> Self {
+        RoomNetwork::Matrix
+    }
+}
+
+impl Serialize for RoomNetwork {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state;
+        match self {
+            Self::Matrix => {
+                state = serializer.serialize_struct("RoomNetwork", 0)?;
+            }
+            Self::All => {
+                state = serializer.serialize_struct("RoomNetwork", 1)?;
+                state.serialize_field("include_all_networks", &true)?;
+            }
+            Self::ThirdParty(network) => {
+                state = serializer.serialize_struct("RoomNetwork", 1)?;
+                state.serialize_field("third_party_instance_id", network)?;
+            }
+        }
+        state.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for RoomNetwork {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_map(RoomNetworkVisitor)
+    }
+}
+
+struct RoomNetworkVisitor;
+impl<'de> Visitor<'de> for RoomNetworkVisitor {
+    type Value = RoomNetwork;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("Network selection")
+    }
+
+    fn visit_map<M>(self, mut access: M) -> Result<Self::Value, M::Error>
+    where
+        M: MapAccess<'de>,
+    {
+        let mut include_all_networks = false;
+        let mut third_party_instance_id = None;
+        while let Some((key, value)) = access.next_entry::<String, serde_json::Value>()? {
+            match key.as_str() {
+                "include_all_networks" => {
+                    include_all_networks = match value.as_bool() {
+                        Some(b) => b,
+                        _ => false,
+                    }
+                }
+                "third_party_instance_id" => {
+                    third_party_instance_id = value.as_str().map(|v| v.to_owned())
+                }
+                _ => {}
+            };
+        }
+
+        if include_all_networks {
+            if third_party_instance_id.is_none() {
+                Ok(RoomNetwork::All)
+            } else {
+                Err(M::Error::custom(
+                    "`include_all_networks = true` and `third_party_instance_id` are mutually exclusive.",
+                ))
+            }
+        } else {
+            Ok(match third_party_instance_id {
+                Some(network) => RoomNetwork::ThirdParty(network),
+                None => RoomNetwork::Matrix,
+            })
+        }
+    }
 }
